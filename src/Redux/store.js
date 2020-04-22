1e1b48d2ada2d7a6d313c8daf1ed0df29c0413bf
Heaven32 <artemtsvelov@gmail.com>
2020-04-22 12:36:18 +0300

    Presentation and Container Components

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,64 +1,64 @@
-import profileReducer from "./profile-reducer";
-import dialogsReducer from "./dialogs-reducer";
-import sidebarReducer from "./sidebar-reducer";
+// import profileReducer from "./profile-reducer";
+// import dialogsReducer from "./dialogs-reducer";
+// import sidebarReducer from "./sidebar-reducer";
 
-let store = {
-    _state : { 
-        profilePage:{ 
-            posts: [
-                {id: 1, message: "Hello people!", likesCount:"23"},
-                {id: 2, message: "How are you bro?", likesCount:"244"},
-                {id: 3, message: "I am fine? thanks", likesCount:"11"},
-                {id: 4, message: "Maybe you want ice", likesCount:"223"},
-                {id: 5, message: "No, it`s realy cold oll", likesCount:"323"},
-                {id: 6, message: "Ha-ha", likesCount:"3"}
-            ],
-            newPostText: ""
-        },
-        dialogsPage:{
-            dialogs: [
-                {id: 1, name: "Jonny"},
-                {id: 2, name: "Tom"},
-                {id: 3, name: "Dina"},
-                {id: 4, name: "Mayke"},
-                {id: 5, name: "Jessy"},
-                {id: 6, name: "Lina"}
-            ],
-            messages: [
-                {id: 1, message: "Hello people!"},
-                {id: 2, message: "How are you bro?"},
-                {id: 3, message: "I am fine? thanks"},
-                {id: 4, message: "Maybe you want ice"},
-                {id: 5, message: "No, it`s realy cold oll"},
-                {id: 6, message: "Ha-ha"}
-            ],
-            newMessageText: ""
-        },
-        sidebar:{
-            blockFriends:[
-                {id: 1, name: "Jonny"},
-                {id: 2, name: "Tom"},
-                {id: 3, name: "Dina"}
-            ]  
-        }
-    },
-    _callSubscriber() {
+// let store = {
+//     _state : { 
+//         profilePage:{ 
+//             posts: [
+//                 {id: 1, message: "Hello people!", likesCount:"23"},
+//                 {id: 2, message: "How are you bro?", likesCount:"244"},
+//                 {id: 3, message: "I am fine? thanks", likesCount:"11"},
+//                 {id: 4, message: "Maybe you want ice", likesCount:"223"},
+//                 {id: 5, message: "No, it`s realy cold oll", likesCount:"323"},
+//                 {id: 6, message: "Ha-ha", likesCount:"3"}
+//             ],
+//             newPostText: ""
+//         },
+//         dialogsPage:{
+//             dialogs: [
+//                 {id: 1, name: "Jonny"},
+//                 {id: 2, name: "Tom"},
+//                 {id: 3, name: "Dina"},
+//                 {id: 4, name: "Mayke"},
+//                 {id: 5, name: "Jessy"},
+//                 {id: 6, name: "Lina"}
+//             ],
+//             messages: [
+//                 {id: 1, message: "Hello people!"},
+//                 {id: 2, message: "How are you bro?"},
+//                 {id: 3, message: "I am fine? thanks"},
+//                 {id: 4, message: "Maybe you want ice"},
+//                 {id: 5, message: "No, it`s realy cold oll"},
+//                 {id: 6, message: "Ha-ha"}
+//             ],
+//             newMessageText: ""
+//         },
+//         sidebar:{
+//             blockFriends:[
+//                 {id: 1, name: "Jonny"},
+//                 {id: 2, name: "Tom"},
+//                 {id: 3, name: "Dina"}
+//             ]  
+//         }
+//     },
+//     _callSubscriber() {
     
-    },
+//     },
 
-    getState() {
-        return this._state; 
-    },
-    subscribe(observer) {
-        this._callSubscriber = observer;
-    },
-    dispatch(action) {
-        this._state.profilePage = profileReducer(this._state.profilePage, action);
-        this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
-        this._state.sidebar = sidebarReducer(this._state.sidebar, action);
+//     getState() {
+//         return this._state; 
+//     },
+//     subscribe(observer) {
+//         this._callSubscriber = observer;
+//     },
+//     dispatch(action) {
+//         this._state.profilePage = profileReducer(this._state.profilePage, action);
+//         this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
+//         this._state.sidebar = sidebarReducer(this._state.sidebar, action);
 
-        this._callSubscriber(this._state)
-    }
-};
+//         this._callSubscriber(this._state)
+//     }
+// };
 
-export default store;
+// export default store;
